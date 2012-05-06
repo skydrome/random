a78a7497c19bd517f2be28c5d08f2ba51cecb02b
Voruse <user@email.net>
2012-05-05 23:09:13 -0400

    comment destroydrive.sh a bit more

--- destroydrive.sh
+++ destroydrive.sh
@@ -8,15 +8,31 @@ if [[ ! $(type -P "openssl") || \
  HDPARM
  PV"
     exit 1
+else
+    echo "
+        # NOTE: http://billauer.co.il/frandom.html
+        #       http://www.issihosts.com/haveged
+        # if you have a large hard drive, you will want to use frandom
+        # along with running the haveged daemon for a great speed boost
+        "
+    echo -n "Continuing in 10 seconds... "; i=9
+    while (($i > 0)); do echo -en "\b$i.\b \b"; sleep 1; ((i--)); done
 fi
+exit
 
 if [[ "$1" = /dev/* && -b "$1" ]]; then
-        # NOTE: http://billauer.co.il/frandom.html
-        # if you have a large hard drive, you may want to try frandom
-        # it claims to be 10-50x faster
         key=$(tr -cd '[:graph:] ' < /dev/urandom | head -c 48)
         openssl enc -aes256 -k "$key" < /dev/urandom | pv -trb > "$1"
+        # reset drive configuration back to factory defaults
         hdparm --yes-i-know-what-i-am-doing --dco-restore "$1"
 else
     echo -e "Please supply a valid hard drive to wipe: EX: /dev/sda \n"
 fi
+
+# If you need to unlock your drive:
+#hdparm --user-master u --security-set-pass p <device>
+#hdparm --user-master u --security-unlock p   <device>
+#hdparm --user-master u --security-disable p  <device>
+
+# If your drive supports secure erase:
+#hdparm --user-master u --security-erase p <device>
