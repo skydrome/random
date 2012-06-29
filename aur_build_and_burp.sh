d4c9a6370fe1fa71c31416e83f6aa3cbdd6c6f6b
voruse <voruse@hush.ai>
2012-06-29 01:33:01 -0400

    run namcap too

--- aur_build_and_burp.sh
+++ aur_build_and_burp.sh
@@ -3,15 +3,16 @@
 for d in $*; do 
 (   cd $d
         echo -en "\nBuilding source... "
-            makepkg --source -f &>/dev/null; ERR=$?
+            namcap PKGBUILD
             source PKGBUILD
-            if (( $ERR != 0 )); then
+            makepkg --source -f &>/dev/null
+            if (( $? != 0 )); then
                 echo "FAIL | $pkgname $pkgver-$pkgrel"
                 break
             fi
         echo "done | $pkgname $pkgver-$pkgrel"
-        #echo "${pkgname}-${pkgver}-${pkgrel}.src.tar.gz has been successfully uploaded (fake)"
-        burp "${pkgname}-${pkgver}-${pkgrel}.src.tar.gz"
+        echo "${pkgname}-${pkgver}-${pkgrel}.src.tar.gz has been successfully uploaded (fake)"
+        #burp "${pkgname}-${pkgver}-${pkgrel}.src.tar.gz"
     cd ..
 )
 done
