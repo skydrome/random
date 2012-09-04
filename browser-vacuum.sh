7b0b3b5fbbc7dfcc20974eb11c934213fecb16b2
voruse <voruse@hush.ai>
2012-09-03 21:49:41 -0400

    no xargs

--- browser-vacuum.sh
+++ browser-vacuum.sh
@@ -2,17 +2,18 @@
 
 RED="\e[01;31m" GRN="\e[01;32m" YLW="\e[01;33m" RST="\e[00m"
 
-_worker() {
-RED="\e[01;31m" GRN="\e[01;32m" YLW="\e[01;33m" RST="\e[00m"
-if [[ $(file $1 | grep SQL | cut -f1 -d:) ]]; then
-    echo -en "${GRN} Cleaning${RST} $1"
-    # Record size of each db before and after vacuuming
-    s_old=$(stat -c%s "$1")
-    (sqlite3 "$1" "VACUUM;" && sqlite3 "$1" "REINDEX;")
-    s_new=$(stat -c%s "$1")
-    diff=$(((s_old - s_new) / 1024))
-    echo -e "$(tput cr)$(tput cuf 36)${GRN}done${RST}  [ -${YLW}${diff}${RST} KB ]"
-fi
+run_cleaner() {
+    while read -rd '' db; do
+    if [[ $(file "$db" | grep SQLite | cut -f1 -d:) ]]; then
+        echo -en "${GRN} Cleaning${RST} $db"
+        # Record size of each db before and after vacuuming
+        s_old=$(stat -c%s "$db")
+        sqlite3 "$1" "VACUUM;" && sqlite3 "$db" "REINDEX;"
+        s_new=$(stat -c%s "$db")
+        diff=$(((s_old - s_new) / 1024))
+        echo -e "$(tput cr)$(tput cuf 38)${GRN}done${RST}  [ -${YLW}${diff}${RST} KB ]"
+    fi
+    done < <(find . -maxdepth 1 -type f -print0)
 }
 
 _running() {
@@ -34,7 +35,7 @@ _firefox() {
             # Figure out the profiles name
             for profiledir in $(grep Path $dir/.mozilla/firefox/profiles.ini | sed 's/Path=//'); do
                 cd $dir/.mozilla/firefox/$profiledir
-                find . -maxdepth 1 -name '*.sqlite' -print0 | xargs -0 -n1 -I{} bash -c "_worker {}"
+                run_cleaner
             done
         else
             echo -e "[${RED}none${RST}]"
@@ -46,17 +47,16 @@ _chromium() {
     _running 'chromium'
     # Check for a .config/chromium folder in each users home directory
     for dir in $(cat /etc/passwd | grep "home" | cut -d':' -f6); do
-        echo -en "\n${GRN}Scanning for chromium profiles in ${YLW}${dir}${RST}  "
+        echo -en "\n${GRN}Scanning for chromium profiles in ${YLW}${dir}${RST} "
         if [[ -d "$dir/.config/chromium/Default" ]]; then
             echo -e "[${GRN}found${RST}]"
             cd $dir/.config/chromium/Default
-            find . -maxdepth 1 -type f -print0 | xargs -0 -n1 -I{} bash -c "_worker {}"
+            run_cleaner
         else
             echo -e "[${RED}none${RST}]"
         fi
     done
 }
 
-export -f _worker
 _firefox
 _chromium
