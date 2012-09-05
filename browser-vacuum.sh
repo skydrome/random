8092c463e6096671db8e9f18a25c48603e2571f9
voruse <voruse@hush.ai>
2012-09-05 02:59:48 -0400

    cleaner/faster way of checking if running first

--- browser-vacuum.sh
+++ browser-vacuum.sh
@@ -16,17 +16,17 @@ run_cleaner() {
     done < <(find . -maxdepth 1 -type f -print0)
 }
 
-_running() {
+if_running() {
+    # TODO: pgrep against $USER from /home/$USER
     # Check if browser is running, wait for it to exit
-    [[ $(ps aux | grep "$1" | grep -v 'grep' | grep -v 'vacuum') ]] &&
+    [[ $(ps aux | grep "$1" | grep -v 'grep') ]] &&
         echo -n "Waiting for "$1" to exit"
-    while [[ $(ps aux | grep "$1" | grep -v 'grep' | grep -v 'vacuum') ]]; do
+    while [[ $(ps aux | grep "$1" | grep -v 'grep') ]]; do
         echo -n "."; sleep 2
     done
 }
 
 _firefox() {
-    _running 'firefox'
     # Check for a .mozilla folder in each users home directory
     for dir in $(cat /etc/passwd | grep "home" | cut -d':' -f6); do
         echo -en "\n${GRN}Scanning for firefox profiles in ${YLW}${dir}${RST}  "
@@ -35,6 +35,7 @@ _firefox() {
             # Figure out the profiles name
             for profiledir in $(grep Path $dir/.mozilla/firefox/profiles.ini | sed 's/Path=//'); do
                 cd $dir/.mozilla/firefox/$profiledir
+                if_running 'firefox'
                 run_cleaner
             done
         else
@@ -44,13 +45,13 @@ _firefox() {
 }
 
 _chromium() {
-    _running 'chromium'
     # Check for a .config/chromium folder in each users home directory
     for dir in $(cat /etc/passwd | grep "home" | cut -d':' -f6); do
         echo -en "\n${GRN}Scanning for chromium profiles in ${YLW}${dir}${RST} "
         if [[ -d "$dir/.config/chromium/Default" ]]; then
             echo -e "[${GRN}found${RST}]"
             cd $dir/.config/chromium/Default
+            if_running 'chromium'
             run_cleaner
         else
             echo -e "[${RED}none${RST}]"
