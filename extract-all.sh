a052af59dfad36ce696c06910c1e781234cabef3
voruse <voruse@hush.ai>
2012-09-06 02:16:00 -0400

    extract-all: add some more archive formats this is really uneeded as most shell completion packages ship their own version of this

--- extract-all.sh
+++ extract-all.sh
@@ -2,17 +2,19 @@
 
 extract() {  # find type of compression and extract accordingly
     case "$1" in
-        *.tar.bz2) tar xjf    "$1" ;;
-        *.tbz2   ) tar xjf    "$1" ;;
-        *.tar.gz ) tar xzf    "$1" ;;
-        *.tgz    ) tar xzf    "$1" ;;
-        *.tar    ) tar xf     "$1" ;;
-        *.gz     ) gunzip -q  "$1" ;;
-        *.bz2    ) bunzip2 -q "$1" ;;
-        *.rar    ) unrar x    "$1" ;;
-        *.zip    ) unzip      "$1" ;;
-        *.Z      ) uncompress "$1" ;;
-        *.7z     ) 7z x       "$1" ;;
+        *.tar.bz2|*.tbz|*.tbz2) tar xjf "$1" ;;
+        *.tar.gz|*.tgz ) tar xzf "$1" ;;
+        *.tar.xz|*.txz ) tar --xz -xf "$1" || xzcat "$1" | tar xvf - ;;
+        *.tar.zma|*.tlz) tar --lzma -xvf "$1" || lzcat "$1" | tar xvf - ;;
+        *.tar ) tar xf      "$1" ;;
+        *.gz  ) gunzip -q   "$1" ;;
+        *.bz2 ) bunzip2 -q  "$1" ;;
+        *.xz  ) unxz        "$1" ;;
+        *.lzma) unlzma      "$1" ;;
+        *.rar ) unrar e -ad "$1" ;;
+        *.zip ) unzip       "$1" ;;
+        *.Z   ) uncompress  "$1" ;;
+        *.7z  ) 7za x       "$1" ;;
     esac
 }
 
