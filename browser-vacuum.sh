4974f12e83f401a313a977d571c8e6b950988d2f
voruse <voruse@hush.ai>
2012-09-17 01:37:19 -0400

    trap terminate signals so we dont exit halfway through a vacuum ask to kill running browsers instead of just giving up

--- browser-vacuum.sh
+++ browser-vacuum.sh
@@ -4,6 +4,8 @@ run_cleaner() {
     while read -rd '' db; do
         # for each file that is an sqlite database vacuum and reindex
         if [[ $(file "$db" | grep SQLite | cut -f1 -d:) ]]; then
+        (
+            trap '' INT TERM
             echo -en "${GRN} Cleaning${RST} $db"
             # Record size of each db before and after vacuuming
             s_old=$(stat -c%s "$db")
@@ -13,68 +15,32 @@ run_cleaner() {
             # convert to kilobytes
             diff=$(((s_old - s_new) / 1024))
             echo -e "$(tput cr)$(tput cuf 40)${GRN}done${RST} [ -${YLW}${diff}${RST} KB ]"
+        )
     fi
     done < <(find . -maxdepth 1 -type f -print0)
 }
 
 if_running() {
-    i=10 # after this timeout, we give up waiting (i*2 seconds)
+    i=7 # after this timeout, we give up waiting (i*2 seconds)
     [[ $(ps aux | grep -v 'grep' | grep "$1" | grep "$user") ]] &&
         echo -n "Waiting for "$1" to exit"
     # Wait for <user's> <browser> to die
     while [[ $(ps aux | grep "$1" | grep -v 'grep') ]];do
         if (( $i == 0 )); then
-            echo "giving up"
-            return 1
+            # waited long enough, ask to kill it
+            read -p " kill it? [y|n]: " ans
+            if [[ "$ans" = @(y|Y|yes) ]]; then
+                kill -TERM $(pgrep -u "$user" "$1")
+                sleep 4
+                # if still running, give monzy the microphone (stanford killdashnine)
+                [[ $(ps aux | grep -v 'grep' | grep "$1" | grep "$user") ]] &&
+                    kill -KILL $(pgrep -u "$user" "$1")
+                break
+            fi
         fi
         echo -n "."; sleep 2
         # tick tock
         ((i--))
-    done
-}
-
-_firefox() {
-    # Check for a <browser config> folder in each users home directory
-    for b in {firefox,icecat,seamonkey}; do
-        echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for $b profiles${RST}"
-        if [[ -f "/home/$user/.mozilla/$b/profiles.ini" ]]; then
-            echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
-            # We found one, now run the cleaner for each <browser profile>
-            for profiledir in $(grep Path /home/$user/.mozilla/$b/profiles.ini | sed 's/Path=//'); do
-                cd /home/$user/.mozilla/$b/$profiledir
-                # Check if <browser> is *not* running before cleaning
-                if_running "$b" && run_cleaner
-            done
-        else
-            # This user has no <browser config>
-            echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
-        fi
-    done
-}
-
-_thunderbird() {
-    echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for thunderbird profiles${RST}"
-    if [[ -f "/home/$user/.thunderbird/profiles.ini" ]]; then
-        echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
-        for profiledir in $(grep Path /home/$user/.thunderbird/profiles.ini | sed 's/Path=//'); do
-            cd /home/$user/.thunderbird/$profiledir
-            if_running 'thunderbird' && run_cleaner
-        done
-    else
-        echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
-    fi
-}
-
-_chromium() {
-    for b in {chromium,google-chrome}; do
-        echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for $b profiles${RST}"
-        if [[ -d "/home/$user/.config/$b/Default" ]]; then
-            echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
-            cd /home/$user/.config/$b/Default
-            if_running "$b" && run_cleaner
-        else
-            echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
-        fi
     done
 }
 
@@ -92,7 +58,45 @@ priv="$USER"
     priv=$(find /home -maxdepth 1 -type d | tail -n+2 | cut -d':' -f6 | cut -c7-)
 
 for user in $priv; do
-    _firefox
-    _thunderbird
-    _chromium
+#[ FIREFOX ICECAT SEAMONKEY ]#
+    # Check for a <browser config> folder in each users home directory
+    for b in {firefox,icecat,seamonkey}; do
+        echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for $b profiles${RST}"
+        if [[ -f "/home/$user/.mozilla/$b/profiles.ini" ]]; then
+            echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
+            # We found one, now run the cleaner for each <browser profile>
+            for profiledir in $(grep Path /home/$user/.mozilla/$b/profiles.ini | sed 's/Path=//'); do
+                cd /home/$user/.mozilla/$b/$profiledir
+                # Check if <browser> is *not* running before cleaning
+                if_running "$b" && run_cleaner
+            done
+        else
+            # This user has no <browser config>
+            echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
+        fi
+    done
+
+#[ THUNDERBIRD ]#
+    echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for thunderbird profiles${RST}"
+    if [[ -f "/home/$user/.thunderbird/profiles.ini" ]]; then
+        echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
+        for profiledir in $(grep Path /home/$user/.thunderbird/profiles.ini | sed 's/Path=//'); do
+            cd /home/$user/.thunderbird/$profiledir
+            if_running 'thunderbird' && run_cleaner
+        done
+    else
+        echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
+    fi
+
+#[ CHROMIUM GOOGLE-CHROME ]#
+    for b in {chromium,google-chrome}; do
+        echo -en "\n[${YLW}$user${RST}] ${GRN}Scanning for $b profiles${RST}"
+        if [[ -d "/home/$user/.config/$b/Default" ]]; then
+            echo -e "$(tput cr)$(tput cuf 45) [${GRN}found${RST}]"
+            cd /home/$user/.config/$b/Default
+            if_running "$b" && run_cleaner
+        else
+            echo -e "$(tput cr)$(tput cuf 45) [${RED}none${RST}]"
+        fi
+    done
 done
