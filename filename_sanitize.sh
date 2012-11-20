21e5f9e8d2601fe3bd39db23af8d46e9cd6114c0
skydrome <skydrome@tormail.net>
2012-11-19 23:53:02 -0500

    sanitation: lowercase and some more deletions

--- filename_sanitize.sh
+++ filename_sanitize.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-for f in *; do
-    file=$(echo $f | \
-                tr A-Z a-z | \
-                tr ' ' _   | \
-                tr '(' _   | \
-                tr ')' _   | \
-                tr ',' _   | \
-                tr '[' _   | \
-                tr  \' _   | \
-                tr ']' _
-            )
-    [[ ! -f $file ]] && mv "$f" $file
+for f in $*; do
+    file=$(echo "$f" \
+        | tr A-Z a-z \
+        | tr ' ' '_' \
+        | tr -d '()[]{},?!' \
+        | tr -d "'" \
+        | tr -d '\' \
+        | tr '[[:upper:]]' '[[:lower:]]' \
+        | sed 's/__/_/g' \
+        | sed 's/_-_/-/g' \
+    )
+    [[ ! -f "$file" ]] && mv -v "$f" "$file"
 done
+exit 0
