d57e458ff58141c049e65cda73ee7a74ad31b053
skydrome <skydrome@tormail.net>
2013-03-04 18:23:04 -0500

    i2p-git_export: made a bit more friendly

--- shell/i2p-mtn_gitexport.sh
+++ shell/i2p-mtn_gitexport.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 # if you wanna touch the sky you must be prepared to die
 
-MTN_SRC="/usr/src/i2p"
-GIT_SRC="$HOME/github/i2p.i2p"
+WORKING="$HOME/src/github"
 MTN_KEY=""
-OPTIONS="--refs=revs --log-revids --log-certs"
 
-cd "$MTN_SRC"
+bootstrap() {
+    mkdir -p "$WORKING" && cd $_ || exit 1
+    mtn db init --db=i2p.mtn
+    mtn --db=i2p.mtn read < $HOME/.monotone/dev_keys
+    mtn --db=i2p.mtn au pull "mtn.i2p-projekt.de?i2p.i2p"
+    mkdir i2p.git && cd $_
+    git init
+    git remote add origin git@github.com:skydrome/i2p.i2p.git
+}
+[[ $1 = bootstrap ]] && bootstrap
+
+cd "$WORKING"
 md5sum i2p.mtn > MD5SUM
-
-    cd i2p.i2p
-    mtn pull -k "$KEY" && mtn up ||
-        { echo "non zero exit status from mtn pull"; exit 1; }
-    cd ..
-
-md5sum --check --status MD5SUM ||
+mtn --db=i2p.mtn au pull "mtn.i2p-projekt.de?i2p.i2p"
+md5sum --check --status MD5SUM &&
 {
-    cd "$GIT_SRC"
-    mtn --db "${MTN_SRC}/i2p.mtn" git_export "$OPTIONS" | git fast-import && git push ||
-            { echo "non zero exit status from git-import/push"; exit 1; }
+    cd "i2p.git"
+    mtn --db "${WORKING}/i2p.mtn" git_export | git fast-import
+    [[ $1 = bootstrap ]] && git checkout i2p.i2p
+    git push origin HEAD:master
 }
