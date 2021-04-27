1e456afff64d9e7e66c0ab912f0d11a1bc1b6624
skydrome <skydrome@protonmail.com>
2021-04-27 15:20:44 -0400

    update xchat cert generator

--- shell/make_xchat_cert.sh
+++ shell/make_xchat_cert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 CERT_NAME="$1"
-XCHAT_DIR=("$2"
-           "$HOME/.xchat"
-           "$HOME/.xchat2"
-           "$HOME/.config/hexchat/certs")
+XCHAT_DIR=(
+    "$2"
+    "$HOME/.config/hexchat/certs"
+)
 
 SUBJECT="/C=AN/ST=ON/L=YM/O=OUS/CN=$HOSTNAME/emailAddress=anon@mous"
 
@@ -18,8 +18,8 @@ for configdir in ${XCHAT_DIR[@]}; do
 done
 
 if [[ ! -d "$configdir" ]]; then
-    echo "Cant find your xchat config. Specify it manually:"
-    echo " ./$(basename $0) [Network Name] [Path to XChat Config Dir]"
+    echo "Cant find your hexchat config. Specify it manually:"
+    echo " ./$(basename $0) [Network Name] [Path to HexChat Config Dir]"
     exit 1
 fi
 
@@ -29,7 +29,8 @@ if [[ -f "$configdir/$1.pem" ]]; then
 fi
 
 cd $(mktemp -d)
-    openssl req -newkey rsa:2048 -nodes -days 365 -x509 -keyout tmp.key -out tmp.cert -subj "$SUBJECT"
+    # 10yrs
+    openssl req -newkey rsa:4096 -nodes -days 3650 -x509 -keyout tmp.key -out tmp.cert -subj "$SUBJECT"
     if [[ $? = 0 ]]; then
         echo "writing certificate to '$CERT_NAME.pem'"
         cat tmp.cert tmp.key > "${CERT_NAME}.pem"
@@ -42,4 +43,5 @@ cd $(mktemp -d)
     fi
 
 echo -e "\n----- NICKSERV FINGERPRINT -----"
-echo -e "$(openssl x509 -sha1 -noout -fingerprint -in "$configdir/${CERT_NAME}.pem" |  sed -e 's/^.*=//;s/://g')\n"
+echo -e "SHA1:  $(openssl x509 -noout -fingerprint -in "$configdir/${CERT_NAME}.pem" -sha1   |sed -e 's/^.*=//;s/://g')\n"
+echo -e "SHA256:$(openssl x509 -noout -fingerprint -in "$configdir/${CERT_NAME}.pem" -sha256 |sed -e 's/^.*=//;s/://g')\n"
